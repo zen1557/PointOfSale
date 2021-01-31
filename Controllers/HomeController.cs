039550d43636e3b99d001758fd2d06ad55de2a45
HΛммΛĐ <52107704+hammadmemon456@users.noreply.github.com>
2021-01-31 21:12:36 +0500

    New Project Hope this One is Make my day :)

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,18 +13,12 @@ namespace POS.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Login()
         {
-            ViewBag.Message = "Your application description page.";
-
+            
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+        
     }
 }
